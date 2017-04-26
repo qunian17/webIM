58861ae6ebf93c88f934fb2f4be88a0e3576facd
qunian17 <qunian17@outlook.com>
2017-04-26 21:49:44 +0800

    format some words

--- server/app.js
+++ server/app.js
@@ -3,21 +3,22 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 server.listen(80);
-app.all('/',(req, res,next) => {
-    console.log(req);
-    next(); 
+app.all('/', (req, res, next) => {
+    console.log( req.query );
+     console.log( req.url );
+    next();
 });
 app.get('/', function (req, res) {
     let path = __dirname + '/view/index.html';
-   // console.log(path);
+    // console.log(path);
     res.sendFile(path);
 
 });
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
     // res.render('/view/404.html', {
     //     title: 'No Found'
     // })
-    res.status(404).send('你个傻逼!');
+    res.status(404).send('找不到网页!');
 });
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
